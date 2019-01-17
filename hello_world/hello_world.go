c0bef39fe389256b549bf5a04f3971f87119e9b0
Burak Tutanlar <burak.tutanlar@goeuro.com>
2019-01-17 12:08:26 +0100

    Add sleep before response

--- hello_world/hello_world.go
+++ hello_world/hello_world.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 const (
-  port = ":80"
+	port = ":80"
 )
 
 var calls = 0
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-  calls++
-  fmt.Fprintf(w, "Hello, world! You have called me %d times.\n", calls)
+	calls++
+
+	time.Sleep(40 * time.Second)
+
+	fmt.Fprintf(w, "Hello, world! You have called me %d times.\n", calls)
 }
 
 func init() {
-  fmt.Printf("Started server at http://localhost%v.\n", port)
-  http.HandleFunc("/", HelloWorld)
-  http.ListenAndServe(port, nil)
+	fmt.Printf("Started server at http://localhost%v.\n", port)
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(port, nil)
 }
 
 func main() {}
